f117091bc7a5bdcc46737fdd9cfc3f2aab6ea2ea
GreyWayfarer <l.box@bk.ru>
2021-11-09 01:11:06 +0700

    Improve sentences in correctness.rb

--- lib/correctness.rb
+++ lib/correctness.rb
@@ -7,19 +7,19 @@ class Correctness < Input
     reg_str = '^[c,f,k]$'
     reg_digit = '^[-]?[0-9]+([.][0-9]+)?$'
     unless input.value.match(reg_digit)
-      puts 'incorrect value'
+      puts 'incorrect temperature value, use numbers from 0 to 9'
       param = false
     end
     unless input.from.match(reg_str)
-      puts 'incorrect from'
+      puts 'incorrect from, enter other symbol from c,f,k'
       param = false
     end
     unless input.to.match(reg_str)
-      puts 'incorrect to'
+      puts 'incorrect to, enter other symbol from c,f,k'
       param = false
     end
     if input.to == input.from
-      puts 'TO and FROM do cannot be the same'
+      puts 'TO and FROM do cannot be the same, enter other symbol from c,f,k'
       param = false
     end
     param
